3d8be8cc38211f5d04d04b4b132916b6bb8be3dc
Lisirra <112258095+Lisirra@users.noreply.github.com>
2022-11-02 13:00:36 +0300

    Add files via upload
    урок 30 Основы JS

---
+++ variables.js
@@ -0,0 +1,14 @@
+let myAge = '30';
+let bestfriendsage = '30';
+console.log (myAge);
+let myburthYear = '1992';
+let currentYear = '2022';
+let usdtorub = '61.43';
+
+const myName = 'Анна';
+const MyBestFriendName = 'Tanya';
+const myCity = 'Санкт Петербург';
+const CapitalOfMycountry = 'Москва';
+const descriptionOfMe = 'foxy devil'
+
+console.log(`Меня зовут ${myName}, мне ${myAge} лет. Я из города ${myCity}, и я  ${descriptionOfMe}.`);
